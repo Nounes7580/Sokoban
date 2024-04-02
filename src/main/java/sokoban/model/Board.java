80fd351492b4b70c7361e14f1d32564a43917a2d
Bourouis younes <1403yobourouis@student.epfc.eu>
2024-04-02 15:35:43 +0200

    fix : move dans la bonne direction

--- src/main/java/sokoban/model/Board.java
+++ src/main/java/sokoban/model/Board.java
@@ -253,7 +253,7 @@ public class Board {
 
 
     public enum Direction {
-        UP(-1, 0), DOWN(1, 0), LEFT(0, -1), RIGHT(0, 1);
+        UP(0, -1), DOWN(0, 1), LEFT(-1, 0), RIGHT(1, 0);
 
         private final int deltaRow;
         private final int deltaCol;
